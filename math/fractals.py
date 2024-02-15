8ea6c6f2a9ee97b8bd1a7d929fd60ca6159b8966
parth-pandey7000 <147671693+h6ker2969@users.noreply.github.com>
2024-02-15 16:32:45 -0500

    Thu Feb 15 16:32:44 EST 2024

--- math/fractals.py
+++ math/fractals.py
@@ -1,46 +1,12 @@
-from matplotlib import pyplot as plt 
 import numpy as np
+import turtle
 
-class mandelbrot:
-    from PIL import Image, ImageDraw
-
+def mandelbrot():
+    points = []
     MAX_ITER = 80
-    def mandelbrot(c):
-        MAX_ITER = 80
-        z = 0
-        n = 0
-        while abs(z) <= 2 and n < MAX_ITER:
-            z = z*z + c
-        n += 1
-        return n
+    def f(z, c):
+        return (z**2) + c
+    
 
-    # Image size (pixels)
-    WIDTH = 600
-    HEIGHT = 400
-
-    # Plot window
-    RE_START = -2
-    RE_END = 1
-    IM_START = -1
-    IM_END = 1
-
-    palette = []
-
-    im = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
-    draw = ImageDraw.Draw(im)
-
-    for x in range(0, WIDTH):
-        for y in range(0, HEIGHT):
-            # Convert pixel coordinate to complex number
-            c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
-                    IM_START + (y / HEIGHT) * (IM_END - IM_START))
-            # Compute the number of iterations
-            m = mandelbrot(c)
-            # The color depends on the number of iterations
-            color = 255 - int(m * 255 / MAX_ITER)
-            # Plot the point
-            draw.point([x, y], (color, color, color))
-
-    im.save('output.png', 'PNG')
-
-mandelbrot()    
+    for z in range(MAX_ITER):
+        np
