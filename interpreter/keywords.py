66b5bb395c9e833dcb5b8fd6a811527577dd4e22
parth-pandey7000 <147671693+h6ker2969@users.noreply.github.com>
2024-03-09 12:35:13 -0500

    Sat Mar  9 12:35:13 EST 2024

--- interpreter/keywords.py
+++ interpreter/keywords.py
@@ -1,1 +1,1 @@
-keywords = r'out|any|not|and|int|float|complex|async|class|if|elseif|else|while|true|false|try|except|enum|function|var|const|import|del|inf|calc|type'
+keywords = r'any|not|and|async|class|if|elseif|else|while|true|false|try|except|enum|function|var|const|import|del|type|undefined|null|return|len|from|str|for|foreach|finally|break|switch|continue|or|extends|global|in'
