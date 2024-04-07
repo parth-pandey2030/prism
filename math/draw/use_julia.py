c8301bc64877c5fb4b2acdd231e832a5fd8cbc8a
parth-pandey7000 <147671693+h6ker2969@users.noreply.github.com>
2024-04-07 13:12:29 -0400

    Sun Apr  7 13:12:29 EDT 2024

---
+++ math/draw/use_julia.py
@@ -0,0 +1,7 @@
+import julia
+
+# Start Julia Runtime
+julia.install()
+
+# Import Julia module
+from julia import Base
--- shell/config.sh
+++ shell/config.sh
@@ -5,11 +5,13 @@ sudo apt install python3-pip
 pip install sympy
 pip install cython
 pip install setuptools
+pip install julia
 
 # Module installation using pip3 (command "pip" may need some variation)
 pip3 install sympy
 pip3 install cython
 pip3 install setuptools
+pip3 install julia
 
 # Command Setup
 chmod +x prism
