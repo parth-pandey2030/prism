2a35eca1bcce925726c04bfcd42f946a2a7e0cde
parth-pandey7000 <147671693+h6ker2969@users.noreply.github.com>
2024-04-20 09:46:19 -0400

    Sat Apr 20 09:46:19 EDT 2024

--- math/repeated/sum.py
+++ math/repeated/sum.py
@@ -1,53 +1,15 @@
 # Sigma (summation)
-class sigma:
-    def __init__(self, begin, end, type):
-        self.begin = begin
-        self.end = end
-        self.type = type
-        
+def arithemetic_sum_sequence(upper: int):
+    """
+    This function takes in an upper bound and sums up 
+    all the numbers from 1 to that number.
 
-    # Arithemetic Sum
-    def arithemetic_sum_sequence(self, sequence): 
-        self.fst = fst = sequence[0]
-        self.lst = lst = sequence[-1]
-        self.n = n = len(sequence)
+    Parameters:
+        int: the upper bound
 
-        return n/2 * (fst + lst)
-    
-    def arithemetic_sum_def(self, function, variable):
-        self.f = function(variable)
-        fst = self.f(self.begin)
-        lst = self.f(self.end)
+    Returns: 
+        int: the sum    
+    """
+    return (upper/2) * (1 + upper)
 
-        return self.n/2*(fst+lst)
-    
-    # Geometric Sum 
-    def endless_geometric_sum(self, a, r):
-        self.a = a
-        self.r = r
-        return self.a/(1-self.r)
-    
-    def geometric_series(a, r, n) :  
-        sum = 0
-        i = 0
-        while i < n :
-            sum = sum + a
-            a = a * r
-            i = i + 1
-     
-        return sum
-     
-    def __init__(self, begin, end, type, function = None, variable = None, a = None, r = None, sequence = None):
-        self.type = type
-        if self.type == 'arithemetic_sum_sequence':
-            self.arithemetic_sum_sequence(sequence)
-
-        elif self.type == 'arithemetic_sum_def':
-            self.arithemetic_sum_def(function, variable)
-
-        elif self.type == 'endless_geometric_sum':
-            self.endless_geometric_sum(a, r)
-
-        elif self.type == 'geometric_series':
-            self.geometric_series(a, r, self.end)            
-    
+print(arithemetic_sum_sequence(10))
