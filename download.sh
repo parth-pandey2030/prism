df8846343a275ae5ce7a8907382b9eea4694dc6c
parth-pandey7000 <147671693+h6ker2969@users.noreply.github.com>
2025-07-18 18:49:00 -0400

    Fri Jul 18 18:49:00 EDT 2025

--- download.sh
+++ download.sh
@@ -1,12 +1,104 @@
 #!/bin/bash
 
+# Loading Animation Function
+spinner() {
+    local pid=$1
+    local spin='-\|/'
+    local i=0
+    tput civis 
+
+    while kill -0 $pid 2>/dev/null; do
+        local i=$(((i + 1) % ${#spin}))
+        printf "\r%s" "Loading ${spin:$i:1}"
+        sleep .1
+    done
+
+    tput cnorm 
+    wait $pid
+    return $?
+}
+
+# Function to figure out which operating system is on the computer.
+operating_system() {
+    case "$(uname -s)" in
+    solaris*) return "SOLARIS" ;;
+    darwin*)  return "OSX" ;; 
+    linux*)   return "LINUX" ;;
+    bsd*)     return "BSD" ;;
+    msys*)    return "WINDOWS" ;;
+    cygwin*)  return "ALSO WINDOWS" ;;
+    *)        return "unknown: $OSTYPE" ;;
+    esac
+}
+
+# Function to chekc if Python is installed
+has_python() {
+    python3 --version
+    output=$?
+    if [ $output -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Function to check if PIP is installed (note that pip3 is used)
+has_pip() {
+    pip3 --version
+    output=$?
+    if [ $output -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 echo "Downloading sub-modules.... "
+has_python
+if [ $? -eq 0 ]; then
+    echo "Python is already installed"
+else
+    echo "Installing python..."
+    case $(operating_system) in
+        SOLARIS)
+            pkg install python3
+            ;;
+        OSX)
+            brew install python3
+            ;;
+        LINUX)
+            sudo apt install python3
+            ;;
+        BSD)
+            pkg install python3
+            ;;
+        WINDOWS)
+            choco install python
+            ;;
+        ALSO_WINDOWS)
+            choco install python
+            ;;
+        *)
+            sudo apt install python3
+            ;;
+    esac
+fi
 
+has_pip
+if [ $? -eq 0 ]; then
+    echo "Pip is already installed"
+else
+    echo "Installing pip"
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3 get-pip.py
+    rm get-pip.py
+fi 
 
-echo "Activating prism.... "
+echo "Activating prism.... " 
 chmod +x prism/shell/prism
 echo "Complete"
 
 echo "Converting prism to shell command.... "
+echo "You will need to enter your password:"
 sudo mv prism/shell/prism /usr/local/bin
 echo "Complete"
